1027bcbf6cb0baa3ef6284e93649e01d3010b66b
rishritesh <59690670+rishritesh@users.noreply.github.com>
2022-06-19 00:43:07 +0530

    Add files via upload

---
+++ OopsLab/src/com/services/Email.java
@@ -0,0 +1,12 @@
+package com.services;
+
+public class Email {
+	
+	public void createEmail( String firstName, String lastName,String departmentName){
+		
+		System.out.println("Email--> "+firstName+lastName+"@"+departmentName+".abc.com");
+		
+		
+	}
+
+}
